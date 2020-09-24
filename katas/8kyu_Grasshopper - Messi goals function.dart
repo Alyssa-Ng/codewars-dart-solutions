4256f9165c66b06e07ba9be4f29cfc9589e5e21f
Alyssa-Ng <38221362+Alyssa-Ng@users.noreply.github.com>
2020-09-24 23:42:30 +0800

    Added 8kyu_Grasshopper - Messi goals function Solution

---
+++ katas/8kyu_Grasshopper - Messi goals function.dart
@@ -0,0 +1,3 @@
+//https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+
+int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)=> laLigaGoals + copaDelReyGoals + championsLeagueGoals;
