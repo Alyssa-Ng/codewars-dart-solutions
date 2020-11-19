cedb8639cf3fca4512d0d127a2df360eadb26b08
Alyssa-Ng <38221362+Alyssa-Ng@users.noreply.github.com>
2020-11-19 22:52:29 +0800

    Added 8kyu_Determine offspring sex based on genes XX and XY chromosomes Solution

---
+++ katas/8kyu_Determine offspring sex based on genes XX and XY chromosomes.dart
@@ -0,0 +1,3 @@
+//https://www.codewars.com/kata/56530b444e831334c0000020
+
+String chromosome_check(String sperm) => sperm == 'XY' ? "Congratulations! You're going to have a son." : "Congratulations! You're going to have a daughter.";
